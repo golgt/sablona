a1d068464b6feedd15e9c4d7014294fb0f32b579
golgt <ovco.sk@gmail.com>
2025-03-15 15:15:53 +0100

    sprehladnenie
    pridanie functions.php

---
+++ qna.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="sk">
+<?php include "parts/header.php";?>
+<body>
+ <?php include "parts/navbar.php"; ?>
+
+  <main>
+    <section class="banner">
+      <div class="container text-white">
+        <h1>Q&A</h1>
+      </div>
+    </section>
+    <section class="container">
+      <div class="row">
+        <div class="col-100 text-center">
+          <p><strong><em>Elit culpa id mollit irure sit. Ex ut et ea esse culpa officia ea incididunt elit velit veniam qui. Mollit deserunt culpa incididunt laborum commodo in culpa.</em></strong></p>
+        </div>
+      </div>
+    </section>
+      <section class="container">
+      <div class="accordion">
+        <div class="question">Otázka 1</div>
+        <div class="answer">Odpoveď 1</div>
+      </div>
+      <div class="accordion">
+        <div class="question">Otázka 2</div>
+        <div class="answer">Odpoveď 2</div>
+      </div>
+      <div class="accordion">
+        <div class="question">Otázka 3</div>
+        <div class="answer">Odpoveď 3</div>
+      </div>
+    </section>
+    </section>
+  </div>
+  </main>
+  <?php include "parts/footer.php";?>
+<script src="js/accordion.js"></script>
+<script src="js/menu.js"></script>
+</body>
+</html>
